0fe22c2ba145b32a437dedab1a0d543c13537994
BatmanGautham <96410215+BatmanGautham@users.noreply.github.com>
2024-03-10 17:40:06 +0530

    Create 1.Hello_from_Function.ino
    This Arduino code initializes serial communication and prints "Hello from setup!" once in the setup function. In the loop function, it continuously prints "Hello from loop!".

---
+++ 1.Hello_from_Function.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello from setup!");
+}
+
+void loop() {
+  // Main code here, to run repeatedly:
+  Serial.println("Hello from loop!");
+}
