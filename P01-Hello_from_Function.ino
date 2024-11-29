22a8a73ca1a92e0e3d426f9a84a1c96bd580e7a1
Gautham S <96410215+BatmanGautham@users.noreply.github.com>
2024-11-30 00:49:01 +0530

    Update and rename 1_Hello_from_Function.ino to P01-Hello_from_Function.ino
    The setup() runs once to initialize, and loop() runs repeatedly to print messages to the Serial Monitor at 9600 baud.

--- P01-Hello_from_Function.ino
+++ P01-Hello_from_Function.ino
@@ -1,10 +1,13 @@
-void setup() {
+void setup() 
+{
   // setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello from setup!");
 }
 
-void loop() {
+
+void loop() 
+{
   // Main code here, to run repeatedly:
   Serial.println("Hello from loop!");
 }
